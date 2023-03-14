d8a1558503ad6ac9c68673a5cdc3a7f352452a6a
Devang Manjramkar <devangmanjramkar@gmail.com>
2023-03-14 20:39:48 +0530

    second commit

--- index.js
+++ index.js
@@ -8,7 +8,51 @@ const accessToken = process.env.accessToken;
 const endpoint = 'https://apiconnect.angelbroking.com/rest/secure/angelbroking/order/v1/placeOrder';
 
 fastify.post('/buy', async (request, reply) => {
-  // To hardcode all order details except buy and sell
+  // Define the request options
+  const requestOptions = {
+    url: endpoint,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'X-UserType': 'USER',
+      'X-SourceID': 'WEB',
+      // 'X-ClientLocalIP': 'CLIENT_LOCAL_IP',
+      // 'X-ClientPublicIP': 'CLIENT_PUBLIC_IP',
+      // 'X-MACAddress': 'MAC_ADDRESS',
+      'X-PrivateKey': `${apiKey}`,
+    },
+    json: true,
+    body: {
+      variety: 'NORMAL',
+      tradingsymbol: orderDetails.tradingsymbol,
+      quantity: orderDetails.quantity,
+      exchange: orderDetails.exchange,
+      transaction_type: 'BUY',
+      order_type: 'LIMIT',
+      product: 'MIS',
+      price: 420.0,
+      validity: 'DAY',
+      disclosed_quantity: 0,
+      tag: 'MY_ORDER',
+    },
+  };
+
+  // Make the request to place the buy order
+  request.post(requestOptions, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      reply.code(500).send(error);
+    } else {
+      console.log(body);
+      reply.send(body);
+    }
+  });
+});
+
+fastify.post('/sell', async (request, reply) => {
+  // Get the order details from the request body
+  const orderDetails = request.body;
 
   // Define the request options
   const requestOptions = {
@@ -27,67 +71,15 @@ fastify.post('/buy', async (request, reply) => {
     json: true,
     body: {
       variety: 'NORMAL',
-      tradingsymbol: 'SBIN-EQ',
-      quantity: 10,
-      exchange: 'NSE',
-      transaction_type: 'BUY',
+      tradingsymbol: orderDetails.tradingsymbol,
+      quantity: orderDetails.quantity,
+      exchange: orderDetails.exchange,
+      transaction_type: 'SELL',
       order_type: 'LIMIT',
       product: 'MIS',
       price: 420.0,
       validity: 'DAY',
       disclosed_quantity: 0,
-      trigger_price: 0,
-      squareoff: 0,
-      stoploss: 0,
-      trailing_stoploss: 0,
-      tag: 'MY_ORDER',
-    },
-  };
-
-  // Make the request to place the buy order
-  request.post(requestOptions, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      reply.code(500).send(error);
-    } else {
-      console.log(body);
-      reply.send(body);
-    }
-  });
-});
-
-fastify.post('/sell', async (request, reply) => {
-  const apiKey = 'YOUR_API_KEY';
-  const accessToken = 'YOUR_ACCESS_TOKEN';
-
-  // Get the order details from the request body
-  const orderDetails = request.body;
-
-  // Define the request options
-  const requestOptions = {
-    url: endpoint,
-    headers: {
-      'X-UserType': 'USER',
-      'X-SourceID': 'WEB',
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/json',
-    },
-    json: true,
-    body: {
-      variety: 'NORMAL',
-      tradingsymbol: orderDetails.tradingsymbol,
-      quantity: orderDetails.quantity,
-      exchange: 'NSE',
-      transaction_type: 'SELL',
-      order_type: 'LIMIT',
-      product: 'MIS',
-      price: orderDetails.price,
-      validity: 'DAY',
-      disclosed_quantity: 0,
-      trigger_price: 0,
-      squareoff: 0,
-      stoploss: 0,
-      trailing_stoploss: 0,
       tag: 'MY_ORDER',
     },
   };
